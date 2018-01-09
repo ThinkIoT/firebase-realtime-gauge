8fbbb3071d22ac63430ab58b3d17bdd94d656e70
Kshitij <kshtjkmr35@gmail.com>
2018-01-09 14:45:44 +0530

    Add panel class to elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// @ts-check
 
 var count = 0;
 var THqueue = [];
@@ -17,31 +18,33 @@ var h_avg = document.querySelector('havg');
 var c = document.querySelector('clock');
 
 
-var pad = function(x)
-{
-    return x < 10 ? '0'+x : x;
+var pad = function (x) {
+    return x < 10 ? '0' + x : x;
 }
 
-var ShowClock = function()
-{
+var ShowClock = function () {
     var d = new Date();
     var h = pad(d.getHours());
     var m = pad(d.getMinutes());
     var s = pad(d.getSeconds());
 
-    c.innerHTML = [h,m,s].join(':');
+    c.innerHTML = [h, m, s].join(':');
 }
 
 setInterval(ShowClock, 1000);
-   
-var tmax = 0, tmin = 99, tavg, ttotal = 0;
-var hmax = 0, hmin = 99, havg, htotal = 0;
+
+var tmax = 0,
+    tmin = 99,
+    tavg, ttotal = 0;
+var hmax = 0,
+    hmin = 99,
+    havg, htotal = 0;
 
 var myRef = new Firebase('https://testiot-2018.firebaseio.com/');
-        
-myRef.on('child_changed', function(snapshot){
+
+myRef.on('child_changed', function (snapshot) {
     data = snapshot.val();
-    
+
     var h, t;
     THqueue.push([data.Humidity, data.Temperature]);
     h = THqueue[count][0];
@@ -52,29 +55,25 @@ myRef.on('child_changed', function(snapshot){
 
     count = count + 1;
 
-    tavg = Number(ttotal/count).toFixed(2);
-    havg = Number(htotal/count).toFixed(2);
+    tavg = Number(ttotal / count).toFixed(2);
+    havg = Number(htotal / count).toFixed(2);
 
-    if(t > tmax)
-    {
+    if (t > tmax) {
         tmax = t;
     }
-    if(t < tmin)
-    {
+    if (t < tmin) {
         tmin = t;
     }
-    if(h > hmax)
-    {
+    if (h > hmax) {
         hmax = h;
     }
-    if(h < hmin)
-    {
+    if (h < hmin) {
         hmin = h;
     }
 
     hum.innerHTML = h;
     tem.innerHTML = t;
-    
+
     t_max.innerHTML = tmax;
     t_min.innerHTML = tmin;
     t_avg.innerHTML = tavg;
@@ -83,24 +82,21 @@ myRef.on('child_changed', function(snapshot){
     h_min.innerHTML = hmin;
     h_avg.innerHTML = havg;
 
-    if(count == 11)
-    {
+    if (count == 11) {
         sEle[0] = THqueue.shift();
         ttotal -= sEle[0][1];
         htotal -= sEle[0][0];
-        
+
         count = count - 1;
     }
-    
-    var i,j;
+
+    var i, j;
     var myTable = document.getElementById('myTable');
-    for(i = 1;i <= THqueue.length;i++)
-    {
+    for (i = 1; i <= THqueue.length; i++) {
         myTable.rows[i].cells[0].innerHTML = i;
-        myTable.rows[i].cells[1].innerHTML = THqueue[i-1][0];
-        myTable.rows[i].cells[2].innerHTML = THqueue[i-1][1];
+        myTable.rows[i].cells[1].innerHTML = THqueue[i - 1][0];
+        myTable.rows[i].cells[2].innerHTML = THqueue[i - 1][1];
     }
 
-    
-});
 
+});
